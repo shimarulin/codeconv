693856824d73f0949206d95cbfc8a5a2ec220e5b
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-06 02:40:48 +0300

    feat(@codeconv/mkrelease): get a current version from git

--- packages/@codeconv/mkrelease/lib/getCurrentVersion.js
+++ packages/@codeconv/mkrelease/lib/getCurrentVersion.js
@@ -1,4 +1,20 @@
-const getCurrentVersion = async () => {}
+const gitSemverTags = require('git-semver-tags')
+
+const getCurrentVersion = (initialVersion = '1.0.0') => {
+  return new Promise((resolve, reject) => {
+    gitSemverTags((err, tags) => {
+      if (err) {
+        reject(err)
+      }
+
+      const gitVersion = [
+        ...tags,
+      ].shift()
+
+      resolve(gitVersion || initialVersion)
+    })
+  })
+}
 
 module.exports = {
   getCurrentVersion,
