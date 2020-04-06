f3479a48b387b5632a8d14ca9b63d94f3c17fd30
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-06 23:50:04 +0300

    feat(@codeconv/mkrelease): get a recommended version increment from git commits

--- packages/@codeconv/mkrelease/lib/getIncrementLevel.js
+++ packages/@codeconv/mkrelease/lib/getIncrementLevel.js
@@ -1,4 +1,20 @@
-const getIncrementLevel = async () => {}
+const conventionalRecommendedBump = require('conventional-recommended-bump')
+
+const getIncrementLevel = (path = process.cwd(), preset = 'angular') => {
+  return new Promise((resolve, reject) => {
+    conventionalRecommendedBump({
+      // For every package https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-recommended-bump#path
+      path,
+      preset,
+    }, (error, recommendation) => {
+      if (!error) {
+        resolve(recommendation)
+      } else {
+        reject(error)
+      }
+    })
+  })
+}
 
 module.exports = {
   getIncrementLevel,
