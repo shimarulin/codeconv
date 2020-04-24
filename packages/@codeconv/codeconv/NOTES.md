e4547bd7a037ee9ad88c49e73dcc55cbaea351d5
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-25 02:04:06 +0300

    feat(@codeconv/codeconv): find plugins

---
+++ packages/@codeconv/codeconv/NOTES.md
@@ -0,0 +1,18 @@
+# Notes
+
+## Cache find results
+
+```bash
+codeconv command --no-cache
+codeconv upgrade-cache
+```
+
+## Links
+
+- https://dev.to/int0h/creating-a-cli-for-your-node-js-app-using-typescript-124p
+- https://github.com/int0h/typed-cli
+- https://kronus.me/2018/06/%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BA%D0%BE%D0%BD%D1%81%D0%BE%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F-%D0%BD%D0%B0-%D0%BF/
+- https://github.com/yargs/yargs/issues/1067#issuecomment-379228186
+- https://containership.engineering/creating-an-extensible-cli-in-nodejs-fabfe47c425c
+- https://dev.to/christopherkade/building-a-cli-with-yargs-ip8
+- https://walrus.ai/blog/2019/11/typescript-cli/
--- packages/@codeconv/codeconv/src/index.ts
+++ packages/@codeconv/codeconv/src/index.ts
@@ -1,1 +1,68 @@
-console.log('Hello')
+import * as path from 'path'
+import * as findUp from 'find-up'
+import * as execa from 'execa'
+import * as fg from 'fast-glob'
+import { ExecaReturnValue } from 'execa'
+
+const getStdout = (value: ExecaReturnValue): string => {
+  return value.stdout
+}
+
+const resolveNodeModules = (prefix: string): Promise<string> => {
+  return fg([
+    '**/node_modules',
+  ], {
+    absolute: true,
+    onlyDirectories: true,
+    cwd: prefix,
+    deep: 2,
+  }).then(pathList => pathList[0])
+}
+
+const resolveParentNodeModules = (nodeModulesPath: string): Promise<string> => {
+  return findUp('node_modules', {
+    type: 'directory',
+    cwd: path.resolve(nodeModulesPath, '../../'),
+  }).then(pathItem => pathItem || nodeModulesPath)
+}
+
+const findPlugins = (nodeModulesPath: string): Promise<string[]> => {
+  return fg([
+    '@codeconv/plugin-create',
+    '**/@codeconv/plugin-*',
+    '**/codeconv-plugin-*',
+  ], {
+    absolute: true,
+    onlyDirectories: true,
+    cwd: nodeModulesPath,
+    // deep: 2,
+    followSymbolicLinks: true,
+  })
+}
+
+(async () => {
+  const [
+    local,
+    global,
+  ] = await Promise.all([
+    execa('npm', [
+      'prefix',
+    ])
+      .then(getStdout)
+      .then(resolveNodeModules)
+      .then(resolveParentNodeModules)
+      .then(findPlugins),
+    execa('npm', [
+      'prefix',
+      '-g',
+    ])
+      .then(getStdout)
+      .then(resolveNodeModules)
+      .then(findPlugins),
+  ])
+
+  console.log([
+    ...global,
+    ...local,
+  ])
+})()
