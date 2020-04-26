ea095d7098209103eef1fa7d210322a78a399c23
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-26 22:33:27 +0300

    feat(@codeconv/codeconv): add command line parser

--- packages/@codeconv/codeconv/README.md
+++ packages/@codeconv/codeconv/README.md
@@ -1,6 +1,6 @@
 # `@codeconv/codeconv`
 
-> CodeConv CLI
+> CodeConv CLI core
 
 ## Install
 
@@ -15,6 +15,14 @@ or using yarn:
 ```bash
 yarn add @codeconv/codeconv
 ```
+
+## Usage
+
+CodeConv CLI can be extended with plugins. CodeConv plugin system will resolve plugins automatically, follow by this
+naming convention:
+
+- `@codeconv/plugin-*`
+- `codeconv-plugin-*`
 
 ## License
 
--- packages/@codeconv/codeconv/src/index.ts
+++ packages/@codeconv/codeconv/src/index.ts
@@ -1,98 +1,36 @@
 import * as path from 'path'
-import * as findUp from 'find-up'
-import * as execa from 'execa'
-import * as fg from 'fast-glob'
-import { ExecaReturnValue } from 'execa'
+import * as fs from 'fs'
+import * as yargs from 'yargs'
+import { getModuleList } from './resolver'
 
-export const getStdout = (value: ExecaReturnValue): string => {
-  return value.stdout
-}
-
-export const getSafePathList = (unsafePathList: (string | undefined)[]): string[] => {
-  return unsafePathList
-    .filter((unsafePath): unsafePath is string => typeof unsafePath === 'string')
-}
-
-export const resolveNodeModulesDir = (prefix: string): Promise<string[]> => {
-  return fg([
-    '**/node_modules',
-  ], {
-    absolute: true,
-    onlyDirectories: true,
-    cwd: prefix,
-    deep: 2,
-  })
-}
-
-export const resolveParentNodeModulesDir = (nodeModulesDirList: string[]): Promise<string[]> => {
-  return Promise.all(nodeModulesDirList.map((nodeModulesPath) => {
-    return findUp('node_modules', {
-      type: 'directory',
-      cwd: path.resolve(nodeModulesPath, '../../'),
-    })
-  }))
-    .then((parentNodeModulesDirList) => {
-      return [
-        ...getSafePathList(parentNodeModulesDirList),
-        ...nodeModulesDirList,
-      ]
-    })
-}
-
-export const findModules = (nodeModulesDirList: string[], predicate: string | string[]): Promise<string[]> => {
-  return Promise.all(nodeModulesDirList.map((nodeModulesDir) => {
-    return fg(predicate, {
-      absolute: true,
-      onlyDirectories: true,
-      cwd: nodeModulesDir,
-      followSymbolicLinks: true,
-    })
-  }))
-    .then((modulePathGroupList) => {
-      return modulePathGroupList.reduce((prev, current) => {
-        return [
-          ...prev,
-          ...current,
-        ]
-      })
-    })
-}
-
-export const getModuleList = (predicate: string | string[]): Promise<[string[], string[]]> => {
-  return Promise.all([
-    execa('npm', [
-      'prefix',
-    ])
-      .then(getStdout)
-      .then(resolveNodeModulesDir)
-      .then(resolveParentNodeModulesDir)
-      .then((nodeModulesDirList) => {
-        return findModules(nodeModulesDirList, predicate)
-      }),
-    execa('npm', [
-      'prefix',
-      '-g',
-    ])
-      .then(getStdout)
-      .then(resolveNodeModulesDir)
-      .then((nodeModulesDirList) => {
-        return findModules(nodeModulesDirList, predicate)
-      }),
-  ])
-}
+const project = path.join(__dirname, '../tsconfig.json')
+const dev = fs.existsSync(project)
+const program = yargs
 
 export const run = async (): Promise<void> => {
   const [
     localPlugins,
     globalPlugins,
   ] = await getModuleList([
-    '@codeconv/plugin-create',
     '**/@codeconv/plugin-*',
     '**/codeconv-plugin-*',
   ])
 
-  console.log([
-    globalPlugins,
-    localPlugins,
-  ])
+  const plugins = [
+    ...globalPlugins,
+    ...localPlugins,
+  ]
+
+  plugins.forEach((pluginPath) => {
+    program.commandDir(pluginPath, {
+      recurse: true,
+      extensions: dev ? [
+        'ts',
+      ] : [
+        'js',
+      ],
+    })
+  })
+
+  program.parse()
 }
