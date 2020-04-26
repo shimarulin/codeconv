fc699b6ab20cd1bf5ddffb3adb22b95675e9ab2e
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-26 10:44:14 +0300

    feat(@codeconv/codeconv): utils to resolve plugins

--- packages/@codeconv/codeconv/src/index.ts
+++ packages/@codeconv/codeconv/src/index.ts
@@ -4,11 +4,16 @@ import * as execa from 'execa'
 import * as fg from 'fast-glob'
 import { ExecaReturnValue } from 'execa'
 
-const getStdout = (value: ExecaReturnValue): string => {
+export const getStdout = (value: ExecaReturnValue): string => {
   return value.stdout
 }
 
-const resolveNodeModules = (prefix: string): Promise<string> => {
+export const getSafePathList = (unsafePathList: (string | undefined)[]): string[] => {
+  return unsafePathList
+    .filter((unsafePath): unsafePath is string => typeof unsafePath === 'string')
+}
+
+export const resolveNodeModulesDir = (prefix: string): Promise<string[]> => {
   return fg([
     '**/node_modules',
   ], {
@@ -16,53 +21,78 @@ const resolveNodeModules = (prefix: string): Promise<string> => {
     onlyDirectories: true,
     cwd: prefix,
     deep: 2,
-  }).then(pathList => pathList[0])
-}
-
-const resolveParentNodeModules = (nodeModulesPath: string): Promise<string> => {
-  return findUp('node_modules', {
-    type: 'directory',
-    cwd: path.resolve(nodeModulesPath, '../../'),
-  }).then(pathItem => pathItem || nodeModulesPath)
-}
-
-const findPlugins = (nodeModulesPath: string): Promise<string[]> => {
-  return fg([
-    '@codeconv/plugin-create',
-    '**/@codeconv/plugin-*',
-    '**/codeconv-plugin-*',
-  ], {
-    absolute: true,
-    onlyDirectories: true,
-    cwd: nodeModulesPath,
-    // deep: 2,
-    followSymbolicLinks: true,
   })
 }
 
-(async () => {
-  const [
-    local,
-    global,
-  ] = await Promise.all([
+export const resolveParentNodeModulesDir = (nodeModulesDirList: string[]): Promise<string[]> => {
+  return Promise.all(nodeModulesDirList.map((nodeModulesPath) => {
+    return findUp('node_modules', {
+      type: 'directory',
+      cwd: path.resolve(nodeModulesPath, '../../'),
+    })
+  }))
+    .then((parentNodeModulesDirList) => {
+      return [
+        ...getSafePathList(parentNodeModulesDirList),
+        ...nodeModulesDirList,
+      ]
+    })
+}
+
+export const findModules = (nodeModulesDirList: string[], predicate: string | string[]): Promise<string[]> => {
+  return Promise.all(nodeModulesDirList.map((nodeModulesDir) => {
+    return fg(predicate, {
+      absolute: true,
+      onlyDirectories: true,
+      cwd: nodeModulesDir,
+      followSymbolicLinks: true,
+    })
+  }))
+    .then((modulePathGroupList) => {
+      return modulePathGroupList.reduce((prev, current) => {
+        return [
+          ...prev,
+          ...current,
+        ]
+      })
+    })
+}
+
+export const getModuleList = (predicate: string | string[]): Promise<[string[], string[]]> => {
+  return Promise.all([
     execa('npm', [
       'prefix',
     ])
       .then(getStdout)
-      .then(resolveNodeModules)
-      .then(resolveParentNodeModules)
-      .then(findPlugins),
+      .then(resolveNodeModulesDir)
+      .then(resolveParentNodeModulesDir)
+      .then((nodeModulesDirList) => {
+        return findModules(nodeModulesDirList, predicate)
+      }),
     execa('npm', [
       'prefix',
       '-g',
     ])
       .then(getStdout)
-      .then(resolveNodeModules)
-      .then(findPlugins),
+      .then(resolveNodeModulesDir)
+      .then((nodeModulesDirList) => {
+        return findModules(nodeModulesDirList, predicate)
+      }),
+  ])
+}
+
+export const run = async (): Promise<void> => {
+  const [
+    localPlugins,
+    globalPlugins,
+  ] = await getModuleList([
+    '@codeconv/plugin-create',
+    '**/@codeconv/plugin-*',
+    '**/codeconv-plugin-*',
   ])
 
   console.log([
-    ...global,
-    ...local,
+    globalPlugins,
+    localPlugins,
   ])
-})()
+}
