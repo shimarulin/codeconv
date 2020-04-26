0e3786c0a3e7901f48667fcf5a86d714ec8e16a2
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-26 22:36:41 +0300

    feat(@codeconv/plugin-extend): command module 'extend' template

--- packages/@codeconv/plugin-extend/src/index.ts
+++ packages/@codeconv/plugin-extend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello')
+import { Argv } from 'yargs'
+
+export const command = 'extend'
+export const describe = 'Extend an existing package with a functional template'
+
+export const builder = {}
+
+export const handler = (argv: Argv): void => {
+  console.log('Run command create')
+  console.log(argv)
+}
