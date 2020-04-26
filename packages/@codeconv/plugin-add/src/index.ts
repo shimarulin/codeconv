5961104ca58f287e1e7eb86d05fd04870cacfb4c
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-26 22:35:23 +0300

    feat(@codeconv/plugin-add): command module 'add' template

--- packages/@codeconv/plugin-add/src/index.ts
+++ packages/@codeconv/plugin-add/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello')
+import { Argv } from 'yargs'
+
+export const command = 'add'
+export const describe = 'Add a local package as dependency'
+
+export const builder = {}
+
+export const handler = (argv: Argv): void => {
+  console.log('Run command add')
+  console.log(argv)
+}
