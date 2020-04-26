eda29e4968c52bd9c24fa44955e32bf23e23ed56
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-26 22:36:01 +0300

    feat(@codeconv/plugin-create): command module 'create' template

--- packages/@codeconv/plugin-create/src/index.ts
+++ packages/@codeconv/plugin-create/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello')
+import { Argv } from 'yargs'
+
+export const command = 'create'
+export const describe = 'Create standard project or package from template'
+
+export const builder = {}
+
+export const handler = (argv: Argv): void => {
+  console.log('Run command create')
+  console.log(argv)
+}
