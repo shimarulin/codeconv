6b580c0f1a28cab4dcaca4024d8616936668ae9b
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-26 22:37:21 +0300

    feat(@codeconv/plugin-release): command module 'release' template

--- packages/@codeconv/plugin-release/src/index.ts
+++ packages/@codeconv/plugin-release/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello')
+import { Argv } from 'yargs'
+
+export const command = 'release'
+export const describe = 'Make project release'
+
+export const builder = {}
+
+export const handler = (argv: Argv): void => {
+  console.log('Run command release')
+  console.log(argv)
+}
