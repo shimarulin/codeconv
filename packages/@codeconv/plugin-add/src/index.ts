fbdb6a580c11e9d9d12bd11c259af27b234469fa
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-04-29 02:01:19 +0300

    feat(@codeconv/plugin-add): resolve packages info

--- packages/@codeconv/plugin-add/src/index.ts
+++ packages/@codeconv/plugin-add/src/index.ts
@@ -1,7 +1,7 @@
 import { prompt } from 'inquirer'
 import { Arguments, Options } from 'yargs'
 import { getWorkspace } from 'ultra-runner/lib/workspace'
-import { PackageJsonWithRoot } from 'ultra-runner/lib/package'
+import { PackageJsonWithRoot, getPackage, findUp } from 'ultra-runner/lib/package'
 import { yellow, green } from 'chalk'
 
 interface AddCommandAnswers {
@@ -26,30 +26,43 @@ export const builder: { [key: string]: Options } = {
 
 export const handler = async ({ pkg, to }: Arguments<{pkg?: string; to?: string}>): Promise<void> => {
   const workspace = await getWorkspace()
+  const currentPath = findUp('package.json')
+  const currentPackage = getPackage(currentPath || process.cwd())
+  const rootPackage = getPackage(workspace?.root || process.cwd())
   const packages = workspace ? Array.from(workspace.packages.values()) : []
   const packageChoices = packages
     .map(({ name }) => name)
+    .sort()
 
   const getPackageDefs = (pkgName: string | undefined): PackageJsonWithRoot | undefined =>
     pkgName ? packages.find(({ name }) => name === pkgName) : undefined
-  const getMsgPrefix = (pkgName: string | undefined): string =>
-    pkgName ? `${yellow(`Package "${pkgName}" not found.`)}\n${green('└')} ` : ''
+  const getNotFoundPrefix = (condition: boolean, pkgName: string | undefined): string =>
+    condition && pkgName ? `${yellow(`Package "${pkgName}" not found.`)}\n${green('└')} ` : ''
+  const getSelfInstallPrefix = (condition: boolean, pkgName?: string): string =>
+    condition ? `${yellow(`Can't install ${pkgName ? `the package "${pkgName}"` : 'a package'} as a recursive dependency.`)}\n${green('└')} ` : ''
 
-  const source = getPackageDefs(pkg)
-  const target = getPackageDefs(to)
+  const argumentSource = getPackageDefs(pkg)
+  const argumentTarget = getPackageDefs(to) || (to === rootPackage?.name ? rootPackage : undefined)
+  const isSame = argumentSource?.name === currentPackage?.name
+  const source = !isSame ? argumentSource : undefined
+  const target = to ? argumentTarget : currentPackage
 
   const answers: AddCommandAnswers = await prompt([
     {
       type: 'list',
       name: 'source',
-      message: `${getMsgPrefix(pkg)}Select the package to be add:`,
-      choices: packageChoices,
+      message: `${getNotFoundPrefix(!isSame, pkg)}${getSelfInstallPrefix(isSame, pkg)}Select the package to be add:`,
+      choices (): string[] {
+        return isSame
+          ? packageChoices.filter(item => item !== argumentSource?.name)
+          : packageChoices.filter(item => item !== target?.name)
+      },
       when: !source,
     },
     {
       type: 'list',
       name: 'target',
-      message: `${getMsgPrefix(to)}Select the target package:`,
+      message: `${getNotFoundPrefix(true, to)}Select the target package:`,
       choices (answers): string[] {
         return packageChoices.filter(item => item !== answers.source && item !== source?.name)
       },
@@ -59,9 +72,9 @@ export const handler = async ({ pkg, to }: Arguments<{pkg?: string; to?: string}
 
   const resolvedPackages: AddCommandPackages = {
     source: source || getPackageDefs(answers.source),
-    target: source || getPackageDefs(answers.target),
+    target: target || getPackageDefs(answers.target),
   }
 
   console.log('Run command add')
-  console.log(resolvedPackages)
+  console.log(resolvedPackages.source?.name, resolvedPackages.target?.name)
 }
