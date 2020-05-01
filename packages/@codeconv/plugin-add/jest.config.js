3780b72fb746a8be71ca20b9f498685947ff0dc1
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-05-01 23:04:58 +0300

    test(@codeconv/plugin-add): mar module

--- packages/@codeconv/plugin-add/jest.config.js
+++ packages/@codeconv/plugin-add/jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
   testTimeout: 10000,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: [
+    '<rootDir>',
+  ],
+  moduleNameMapper: {
+    '@codeconv/utils': '<rootDir>/../utils/src',
+  },
 }
