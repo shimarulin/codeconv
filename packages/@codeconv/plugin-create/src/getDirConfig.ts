c7e74bd67a5b2cc19ffbb13425927d5e95f4c152
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-05-05 02:55:56 +0300

    chore(@codeconv/license): init

--- packages/@codeconv/plugin-create/src/getDirConfig.ts
+++
@@ -1,9 +0,0 @@
-type AddCommandParsed = {
-  name: string;
-}
-
-export const getDirInfo = async (): Promise<AddCommandParsed> => {
-  return {
-    name: 'test',
-  }
-}
--- packages/@codeconv/plugin-create/src/getDirInfo.ts
+++
@@ -1,9 +0,0 @@
-type AddCommandParsed = {
-  name: string;
-}
-
-export const getEnvironmentInfo = async (): Promise<AddCommandParsed> => {
-  return {
-    name: 'test',
-  }
-}
