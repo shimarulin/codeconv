e520312b3687707934e745eeaf2d3950815b9bb5
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-12-25 06:12:37 +0300

    docs(@codeconv/plugin-release): add todo

--- packages/@codeconv/plugin-release/src/index.ts
+++ packages/@codeconv/plugin-release/src/index.ts
@@ -6,6 +6,7 @@ export const describe = 'Make project release'
 export const builder: { [key: string]: Options } = {}
 
 export const handler = (argv: Arguments): void => {
+  // TODO: implement tmp/mkrelease
   console.log('Run command release')
   console.log(argv)
 }
