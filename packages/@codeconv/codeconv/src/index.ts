ac9d02688227fc8d66e7acd3e275797388866dd6
Vyacheslav Shimarulin <shimarulin@gmail.com>
2022-05-12 00:19:30 +0300

    refactor: simplify loading command module

--- packages/@codeconv/codeconv/src/index.ts
+++ packages/@codeconv/codeconv/src/index.ts
@@ -1,37 +1,32 @@
 import yargs from 'yargs'
-import { getPackageDirs, getPackageList } from '@codeconv/packages-resolver'
+import { hideBin } from 'yargs/helpers'
+import type { CommandModule } from 'yargs'
+import { getPackageList } from '@codeconv/packages-resolver'
 import { getContext } from '@codeconv/context'
 
 export const run = async (): Promise<void> => {
-  const program = yargs(process.argv.slice(2))
+  const program = yargs(hideBin(process.argv))
     .usage('Usage: $0 <command> [options]')
     .help('h')
     .alias('h', 'help')
 
+  program.command('$0', 'Print this message', {}, async (argv) => {
+    console.info(`${await program.getHelp()}`)
+  })
+
   const context = await getContext()
-  const commandDirs = await getPackageList([
+  const commandModuleNames = await getPackageList([
     '**/@codeconv/plugin-*',
     '**/codeconv-plugin-*',
   ], context.project ? 'local' : 'global')
 
-  console.log(commandDirs)
+  const commandModules = await Promise.all(commandModuleNames.map((commandModuleName) => {
+    return import(commandModuleName) as Promise<CommandModule>
+  }))
 
-  for (const plugin of commandDirs) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { command, describe, builder, handler } = await import(plugin)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    program.command(command, describe, builder, handler)
-  }
-
-  // commandDirs.forEach((pluginPath) => {
-  //   program.commandDir(pluginPath, {
-  //     recurse: true,
-  //     extensions: [
-  //       'js',
-  //     ],
-  //     include: new RegExp(`${pluginPath}/(lib|dist)/.*`),
-  //   })
-  // })
+  commandModules.forEach((commandModule) => {
+    program.command(commandModule)
+  })
 
   await program.parse()
 }
