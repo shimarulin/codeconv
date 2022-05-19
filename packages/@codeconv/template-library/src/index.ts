8e29637ca2552bd592751be0a49c4d7a5547ca8b
Vyacheslav Shimarulin <shimarulin@gmail.com>
2022-05-19 03:10:46 +0300

    refactor: fill template by type

--- packages/@codeconv/template-library/src/index.ts
+++ packages/@codeconv/template-library/src/index.ts
@@ -1,6 +1,8 @@
 import type { TemplateModule } from '@codeconv/plugin-create'
 
 const template: TemplateModule = {
+  name: 'Library',
+  description: 'Create common lib',
   render (args) {
     console.log(args.name)
   },
