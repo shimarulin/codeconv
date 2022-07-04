9b524ddd8c1fe37549802d2de448d4f4ac133fe9
Vyacheslav Shimarulin <shimarulin@gmail.com>
2022-07-05 00:22:06 +0300

    feat: add template engine to common library template

--- packages/@codeconv/template-library/src/index.ts
+++ packages/@codeconv/template-library/src/index.ts
@@ -1,16 +1,54 @@
-import manifestGen from '@codeconv/generator-manifest'
-
 import type { TemplateModule } from '@codeconv/plugin-create'
+import { tpl, TemplateInputOptions } from '@codeconv/tpl'
+import ejsHandler from '@codeconv/tpl-ejs-handler'
 
 const template: TemplateModule = {
   name: 'Library',
   describe: 'Create common lib',
-  handler ({ name }) {
-    const manifest = manifestGen.generate({
-      name,
-    })
+  async handler ({ name }) {
+    /**
+     * if (!name) {
+     *   input name
+     * }
+     * if (ctx.project) {
+     *   select namespace if ctx.namespaces.length > 1
+     * }
+     * */
 
-    console.log(manifest)
+    // const manifest = manifestGen.generate({
+    //   name,
+    // })
+    //
+    // console.log(manifest)
+    console.log(name)
+
+    // const templateFileListItems = await getRecursiveFileList(new URL('../templates/typescript', import.meta.url))
+    //
+    // console.log(templateFileListItems)
+    // console.log(templateFileListItems.map((item) => join(item.root, format(item))))
+
+    const templateInputOptions: TemplateInputOptions = {
+      input: [
+        '/home/shimarulin/Проекты/Tools/codeconv/packages/@codeconv/template-library/templates/javascript',
+        '/home/shimarulin/Проекты/Tools/codeconv/packages/@codeconv/template-library/templates/common',
+      ],
+      output: '/home/shimarulin/Проекты/Tools/codeconv/packages/@codeconv/template-library/out',
+      data: {
+        license: {
+          licenseText: 'MIT is license',
+        },
+        manifest: {
+          name: 'ttt',
+          description: 'description',
+          license: 'MIT',
+        },
+      },
+      handlers: [
+        ejsHandler,
+      ],
+    }
+
+    await tpl(templateInputOptions)
   },
 }
 
