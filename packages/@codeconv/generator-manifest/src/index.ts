11155f596d66efa26a4acbd9551a2909f9af8c99
Vyacheslav Shimarulin <shimarulin@gmail.com>
2022-07-05 00:20:07 +0300

    feat: manifest generator

--- packages/@codeconv/generator-manifest/src/index.ts
+++ packages/@codeconv/generator-manifest/src/index.ts
@@ -11,24 +11,113 @@ export interface Manifest {
   version: string
 }
 
+export interface ManifestDefaults {
+  name?: string;
+  author?: string;
+  email?: string;
+  // license?: string;
+  version?: string;
+}
+
+export interface ManifestAnswers extends Required<ManifestDefaults> {
+  description: string;
+  // publish: boolean;
+}
+
 const { prompt } = inquirer
 
-export const generate = (input: ManifestInput): Manifest => {
-  const name = input.name || 'name'
-  const version = input.version || '1.0.0'
+export const askQuestions = async (defaults: ManifestDefaults): Promise<ManifestAnswers> => {
+  // const name = input.name || 'name'
+  // const version = input.version || '1.0.0'
 
-  const description: InputQuestion = {
+  const name: InputQuestion<ManifestAnswers> = {
+    type: 'input',
+    name: 'name',
+    // message (answers) {
+    //   return `${answers.type ? 'Project' : 'Package'} name${answers.namespace ? ` in "${answers.namespace as string}" namespace` : ''}`
+    // },
+    default: defaults.name,
+    validate (input: string): boolean {
+      // see rules from https://docs.npmjs.com/cli/v8/configuring-npm/package-json#name
+      return input.length > 0 && input.length <= 214
+    },
+  }
+
+  const description: InputQuestion<ManifestAnswers> = {
     type: 'input',
     name: 'description',
     message: 'Project description',
   }
 
-  return {
+  const version: InputQuestion<ManifestAnswers> = {
+    type: 'input',
+    name: 'version',
+    message: 'Project version',
+    default: defaults.version,
+    // when: !overrides.version,
+  }
+
+  const author: InputQuestion<ManifestAnswers> = {
+    type: 'input',
+    name: 'author',
+    message: 'Author name',
+    default: defaults.author,
+  }
+
+  const email: InputQuestion<ManifestAnswers> = {
+    type: 'input',
+    name: 'email',
+    message: 'Author email',
+    default: defaults.email,
+  }
+
+  const answers = await prompt<ManifestAnswers>([
+    // namespace,
+    // type,
     name,
+    description,
     version,
+    author,
+    email,
+    // origin,
+    // license,
+    // publish,
+  ])
+
+  return {
+    ...answers,
   }
 }
 
+// const createManifest = (ctx) => {
+//   const manifest = {
+//     name: answers.namespace ? `${answers.namespace}/${answers.name}` : answers.name,
+//     version: answers.version,
+//     description: answers.description,
+//     license: answers.license,
+//     private: !answers.publish ? true : undefined,
+//     publishConfig: answers.publish
+//       ? {
+//         access: 'public',
+//       }
+//       : undefined,
+//     repository: {
+//       ...gitUrl.repository,
+//     },
+//     bugs: {
+//       ...gitUrl.bugs,
+//     },
+//     scripts: {},
+//     homepage: gitUrl.homepage,
+//     author: `${answers.author} <${answers.email}>`,
+//     workspaces: answers.type === 'monorepo'
+//       ? [
+//         `packages/@${answers.name}/*`,
+//       ]
+//       : undefined,
+//   }
+// }
+
 export default {
-  generate,
+  askQuestions,
 }
